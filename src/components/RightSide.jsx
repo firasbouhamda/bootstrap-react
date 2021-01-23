4053efc7235add874ad6cd08eccc609c8ab53d67
firas <firasbouhamda@gmail.com>
2021-01-23 10:14:46 +0100

    sirst commit

---
+++ src/components/RightSide.jsx
@@ -0,0 +1,12 @@
+import react from 'react';
+import {Image} from "react-bootstrap";
+
+const RightSide = () => {
+    return (
+        <div>
+           <Image src="./img/password.png" thumbnail style={{border:"none"}} /> 
+        </div>
+    )
+}
+
+export default RightSide;
